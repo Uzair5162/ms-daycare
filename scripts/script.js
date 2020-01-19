28e38f6d34965d1859650bb97455ae5c76d8bd9d
Uzair Ahmad <uzair_a@outlook.com>
2020-01-19 12:30:44 -0500

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function getAge() {
+    document.getElementById("yessir").innerHTML = (document.getElementById("ms-severity")).value;
+}
+
+function display() {
+    document.getElementById("yessir").innerHTML = "There is a nany available."
+}
